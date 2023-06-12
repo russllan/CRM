5c13450dc6674d1f5741e870b041e2228bff5f99
MAXONCHIK236 <m-garzhaikin@bk.ru>
2023-06-12 17:44:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin";
+import Driver from "./pages/Driver";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/driver" element={<Driver />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Driver.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Driver = () => {
+  return (
+    <div>
+      Driver Page
+    </div>
+  )
+}
+
+export default Driver
