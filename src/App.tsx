11fb96003188c9b0a0705cc99be9a260ced7fd14
Akbermet198 <122314250+Akbermet198@users.noreply.github.com>
2023-07-04 23:45:11 +0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
 import Driver from "./pages/Driver";
+import { useTranslation } from "react-i18next";
+
 function App() {
+
+  const { t, i18n } = useTranslation(["main"]);
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/driver" element={<Driver />} />
+          {/* <Route path="/admin" element={<Admin />} />
+          <Route path="/driver" element={<Driver />} /> */}
         </Routes>
+        <h1>{t("App.title")}</h1>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      jjjj
       </BrowserRouter>
     </>
   );
