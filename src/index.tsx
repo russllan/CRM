b1d19b440ca272dc0683bced4c83b439ee490e2a
Akbermet198 <122314250+Akbermet198@users.noreply.github.com>
2023-07-06 08:04:43 +0600

    react strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-
-  <I18nextProvider i18n={i18n}>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
-  </I18nextProvider>
-  
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </I18nextProvider>
+  </React.StrictMode>
 );
