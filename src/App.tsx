2ea36e845b393dfe3bc938ac664002d55b91e736
Akbermet198 <122314250+Akbermet198@users.noreply.github.com>
2023-07-08 17:20:08 +0600

    reactStricmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/Admin";
-import Driver from "./pages/Driver";
 import { useTranslation } from "react-i18next";
 
 function App() {
@@ -15,16 +14,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/admin" element={<Admin />} />
-          <Route path="/driver" element={<Driver />} />
-          
+  
         </Routes>
         <h1>{t("App.title")}</h1>
       <button onClick={() => changeLanguage("en")}>EN</button>
       <button onClick={() => changeLanguage("ru")}>RU</button>
-     
-     
-
       </BrowserRouter>
+      jjj
     </>
   );
 }
